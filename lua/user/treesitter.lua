de9e426db4441380abfccf23b20301974d2eaa68
prynnekey <prynnekey@gmail.com>
2022-12-16 22:27:41 +0800

    新增treesitter相关的插件

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup({
-  ensure_installed = { "bash", "c", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "java", "yaml", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
+  ensure_installed = {  }, -- one of "all" or a list of languages
 	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
@@ -14,4 +14,30 @@ configs.setup({
 		enable = true,
 	},
 	indent = { enable = true, disable = { "python", "css" } },
+	rainbow = {
+		enable = true,
+		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		-- colors = {}, -- table of hex strings
+		-- termcolors = {} -- table of colour name strings
+	},
+	playground = {
+		enable = true,
+		disable = {},
+		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+		persist_queries = false, -- Whether the query persists across vim sessions
+		keybindings = {
+		  toggle_query_editor = 'o',
+		  toggle_hl_groups = 'i',
+		  toggle_injected_languages = 't',
+		  toggle_anonymous_nodes = 'a',
+		  toggle_language_display = 'I',
+		  focus_language = 'f',
+		  unfocus_language = 'F',
+		  update = 'R',
+		  goto_node = '<cr>',
+		  show_help = '?',
+		},
+	}
 })
