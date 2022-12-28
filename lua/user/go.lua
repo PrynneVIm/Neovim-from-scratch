a45dacc455af8422116e2ba4b0c6383d1e32c125
prynnekey <prynnekey@gmail.com>
2022-12-28 23:43:41 +0800

    修复:go.lua语法报错

--- lua/user/go.lua
+++ lua/user/go.lua
@@ -3,7 +3,7 @@ vim.g.go_fmt_command = 'goimports'
 vim.g.go_autodetect_gopath = 1
 -- vim g:go_bin_path = '$GOBIN'
 
-vim.g:go_version_warning = 1
+vim.g.go_version_warning = 1
 
 vim.g.go_highlight_types = 1
 vim.g.go_highlight_fields = 1
@@ -15,7 +15,7 @@ vim.g.go_highlight_generate_tags = 1
 vim.g.go_highlight_methods = 1
 
 
-vim.g:godef_split=2
+vim.g.godef_split=2
 
 -- Open :GoDeclsDir with ctrl-g
 -- nmap <C-g> :GoDeclsDir<cr>
