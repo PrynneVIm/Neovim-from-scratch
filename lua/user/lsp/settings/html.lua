a17ae558101689de7940f3a3fe5481621e85ddd6
prynnekey <prynnekey@gmail.com>
2022-12-30 20:26:47 +0800

    新增:html LSP

--- lua/user/lsp/settings/html.lua
+++ lua/user/lsp/settings/html.lua
@@ -1,15 +1,19 @@
 local util = require "lspconfig/util"
-local root_pattern = util.root_pattern("package.json")
 
+local bin_name = 'vscode-html-language-server'
+local cmd = { bin_name, '--stdio' }
 
+if vim.fn.has 'win32' == 1 then
+  cmd = { 'cmd.exe', '/C', bin_name, '--stdio' }
+end
 
 return {
-	cmd = {"html-languageserver", "--stdio"},
+	cmd = cmd,
   filetypes = {"html"},
-  root_dir = function(fname)
-    return root_pattern(fname) or vim.loop.os_homedir()
-  end;
+  root_dir = util.root_pattern('package.json', '.git'),
+  single_file_support = true,
   init_options = {
+    provideFormatter = true,
     configurationSection= {"html", "css", "javascript"},
     embeddedLanguages  = {
       css = true,
