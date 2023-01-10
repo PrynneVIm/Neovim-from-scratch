72c2d71beab1f79aef6f7ac06d9ed73b7bbf960a
prynnekey <prynnekey@gmail.com>
2023-01-10 19:54:45 +0800

    优化:gopls智能提示LSP

--- lua/user/lsp/settings/gopls.lua
+++ lua/user/lsp/settings/gopls.lua
@@ -1,10 +1,13 @@
--- local lspconfig = require "lspconfig"
-local util = require "lspconfig/util"
+local util = require "lspconfig.util"
 
 return {
-	cmd = {"gopls", "serve"},
+	cmd = {"gopls"},
   filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  -- root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  root_dir = function(fname)
+    return util.root_pattern 'go.work'(fname) or util.root_pattern('go.mod', '.git')(fname)
+  end,
+  single_file_support = true,
   settings = {
     gopls = {
       analyses = {
