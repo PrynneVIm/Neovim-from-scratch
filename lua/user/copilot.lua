6b687081c29bfddd3f5be49b098860c6df181ad8
prynnekey <prynnekey@gmail.com>
2023-01-11 15:51:56 +0800

    新增:copilot代理

--- lua/user/copilot.lua
+++ lua/user/copilot.lua
@@ -1,4 +1,7 @@
 -- use this table to disable/enable filetypes
 vim.g.copilot_filetypes = { xml = false }
 
+-- 开启代理
+vim.g.copilot_proxy = 'localhost:7890'
+
 vim.cmd[[highlight CopilotSuggestion guifg=#555555 ctermfg=8]]
