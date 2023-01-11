f8877854246a9f001a9ae53ae2505822bc35db8e
prynnekey <prynnekey@gmail.com>
2023-01-11 16:30:58 +0800

    新增:copilot快捷键

--- lua/user/copilot.lua
+++ lua/user/copilot.lua
@@ -5,3 +5,8 @@ vim.g.copilot_filetypes = { xml = false }
 vim.g.copilot_proxy = 'localhost:7890'
 
 vim.cmd[[highlight CopilotSuggestion guifg=#555555 ctermfg=8]]
+
+-- 快捷键
+vim.g.copilot_assume_mapped = true
+
+-- vim.api.nvim_set_keymap("!", "<C-]>", 'copilot#Accept("<CR>")', { silent = true, expr = true, noremap=true })
