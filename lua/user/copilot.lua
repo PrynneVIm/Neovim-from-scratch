9aef60c45df6a29ffdec67e66db5e6bd52172794
prynnekey <prynnekey@gmail.com>
2023-01-14 12:29:01 +0800

    修改:copilot cmp

--- lua/user/copilot.lua
+++ lua/user/copilot.lua
@@ -68,7 +68,7 @@ copilotCmp.setup {
   method = "getCompletionsCycling",
   formatters = {
     label = require("copilot_cmp.format").format_label_text,
-    insert_text = require("copilot_cmp.format").remove_existing,
+    insert_text = require("copilot_cmp.format").format_insert_text,
     preview = require("copilot_cmp.format").deindent,
   },
 }
