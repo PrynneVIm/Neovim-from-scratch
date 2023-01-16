c5adc3d8ac803dfd3302eb23edc8057a35a0b2fa
prynnekey <prynnekey@gmail.com>
2023-01-16 22:56:27 +0800

    新增:neotest配置

--- lua/user/neotest.lua
+++ lua/user/neotest.lua
@@ -15,17 +15,40 @@ vim.diagnostic.config({
 }, neotest_ns)
 
 neotest.setup({
-  icons ={
+  icons = {
     -- https://www.bilibili.com/video/BV1GD4y1B7Xm/?vd_source=d6bc7040894b607a59071589b402d7f2
-  },
+		-- running_animated = {
+		-- 	"",
+		-- 	"🞅",
+		-- 	"🞈",
+		-- 	"🞉",
+		-- 	"",
+		-- 	"",
+		-- 	"🞉",
+		-- 	"🞈",
+		-- 	"🞅",
+		-- 	"",
+		-- },
+	},
+  diagnostic = {
+		enabled = true,
+	},
+	status = {
+		virtual_text = true,
+		signs = true,
+	},
+	strategies = {
+		integrated = {
+			width = 180,
+		},
+	},
   adapters = {
-    require("neotest-go"),
-    -- require("neotest-go")({
-    --   experimental = {
-    --     test_table = true,
-    --   },
-    --   args = { "-count=1", "-timeout=60s" }
-    -- }),
+		require("neotest-go")({
+			args = { "-count=1", "-timeout=60s", "-race", "-cover" },
+		}),
+		-- require("neotest-python")({
+		-- 	dap = { justMyCode = false, console = "integratedTerminal" },
+		-- }),
     require("neotest-plenary"),
     -- require("neotest-vim-test")({
     --   ignore_file_types = { "python", "vim", "lua" },
