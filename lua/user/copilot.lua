223f344801271999b345a56fcbde772432a0be1f
prynnekey <prynnekey@gmail.com>
2023-01-22 02:17:41 +0800

    优化:copilot

--- lua/user/copilot.lua
+++ lua/user/copilot.lua
@@ -2,12 +2,12 @@
 vim.g.copilot_filetypes = { xml = false }
 
 -- 开启代理
--- vim.g.copilot_proxy = 'localhost:7890'
+vim.g.copilot_proxy = 'localhost:7890'
 
 -- vim.cmd[[highlight CopilotSuggestion guifg=#555555 ctermfg=8]]
 
 -- 快捷键
--- vim.g.copilot_assume_mapped = true
+vim.g.copilot_assume_mapped = true
 vim.api.nvim_set_keymap("!", "<C-]>", 'copilot#Accept("<CR>")', { silent = true, expr = true, noremap=true })
 
 
