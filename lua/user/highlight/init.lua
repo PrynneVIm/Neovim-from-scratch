798ff24380c840237d67b744caae464f5d6c04d5
prynnekey <prynnekey@gmail.com>
2023-05-02 14:07:25 +0800

    Fix plugin load sequence

--- lua/user/highlight/init.lua
+++ lua/user/highlight/init.lua
@@ -1,4 +1,4 @@
-require "user.highlight.hlargs"
-require "user.highlight.indentline"
 require "user.highlight.treesitter"
 require "user.highlight.treesitter-context"
+require "user.highlight.hlargs"
+require "user.highlight.indentline"
