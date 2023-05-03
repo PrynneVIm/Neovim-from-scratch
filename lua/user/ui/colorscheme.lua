210ef8f22cc81573cb1440b98abedc7e316bdd79
prynnekey <prynnekey@gmail.com>
2023-05-03 23:27:58 +0800

    Feat colorscheme

--- lua/user/ui/colorscheme.lua
+++ lua/user/ui/colorscheme.lua
@@ -1,7 +1,12 @@
 local colorscheme = "darkplus"
--- local colorscheme = "onedark"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+
+-- local status_ok, _ = pcall(
+--   function()
+--     vim.cmd [[colorscheme darkplus]]
+--   end
+-- )
 
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. "not found!")
