05a29a94c1f9309cb8e1cf2c7d0b1d899f9f4254
prynnekey <prynnekey@gmail.com>
2023-05-10 11:25:36 +0800

    Remove powershell map

--- lua/user/keymap/whichkey.lua
+++ lua/user/keymap/whichkey.lua
@@ -222,7 +222,7 @@ local mappings = {
     f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
     h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
     v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
-    s = { "<cmd>lua _POWERSHELL_TOGGLE()<cr>", "PowerShell" },
+    -- s = { "<cmd>lua _POWERSHELL_TOGGLE()<cr>", "PowerShell" },
 
     -- 翻译
     w = { "<cmd>TranslateW<cr>", "TranslateW" },
