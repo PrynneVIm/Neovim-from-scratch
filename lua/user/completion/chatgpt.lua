3d3905fd4ba02536d7eafcef89f69cfeb88f9d4f
prynnekey <prynnekey@gmail.com>
2023-05-19 01:27:07 +0800

    Feap chatgpt

--- lua/user/completion/chatgpt.lua
+++ lua/user/completion/chatgpt.lua
@@ -1,8 +1,8 @@
 local status_ok, chatgpt = pcall(require, "chatgpt")
 if not status_ok then
-  return
+	return
 end
 
 chatgpt.setup({
-  -- optional configuration
+	-- api_key_cmd = "op read op://private/OpenAI/credential --no-newline",
 })
