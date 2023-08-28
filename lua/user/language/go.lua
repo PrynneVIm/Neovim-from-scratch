699b2dbd920d596bd852f5d602fe779ce07d5a40
prynnekey <prynnekey@gmail.com>
2023-08-28 20:18:55 +0800

    Remove goctl

--- lua/user/language/go.lua
+++ lua/user/language/go.lua
@@ -1,11 +1,3 @@
--- goctl插件 提供api语法高亮等
-local status_ok_3, goctl = pcall(require, "goctl")
-if not status_ok_3 then
-  return
-end
-
-goctl.setup()
-
 local status_ok, go = pcall(require, "vim-go")
 if not status_ok then
 	return
