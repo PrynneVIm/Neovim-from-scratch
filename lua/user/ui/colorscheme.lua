c8d17f7c1a102d512ccd8c1d5afb9db222800123
prynnekey <prynnekey@gmail.com>
2023-08-28 20:19:24 +0800

    Ignore prompt

--- lua/user/ui/colorscheme.lua
+++ lua/user/ui/colorscheme.lua
@@ -9,6 +9,6 @@ local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 -- )
 
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  -- vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
